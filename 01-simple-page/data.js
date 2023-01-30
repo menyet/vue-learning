2a7cb49d7d11af43580d202d712ea1a53cc413f0
Andrej Mandli <andrej.mandli@betterask.erni>
2023-01-30 10:43:35 +0100

    Show first card

---
+++ 01-simple-page/data.js
@@ -0,0 +1,22 @@
+const cards = [
+    {
+        front: 'Card 1 Front',
+        back: '<b>Card 1</b> back'
+    },
+    {
+        front: 'Card 2 Front',
+        back: '<b>Card 2</b back'
+    },
+    {
+        front: 'Card 3 Front',
+        back: 'Card 3 back'
+    },
+    {
+        front: 'Card 4 Front',
+        back: 'Card 4 back'
+    },
+    {
+        front: 'Card 5 Front',
+        back: 'Card 5 back'
+    }
+]
